fb6ec296a072a23c99ebe5ec63696272f3420fdc
Ramwh <vasilkovd_67@mail.ru>
2022-10-27 21:01:25 +0400

    Добавлено решение второй задачи

---
+++ DZ4.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int getSumOfDigits(int num)
+            {
+                string str = num.ToString();
+                int[] array = new int[str.Length];
+                int sum = 0;
+                for (int i=0; i<str.Length; i++)
+                {
+                    array[i] = int.Parse(str[i].ToString());
+                    sum = sum + array[i];
+                }
+                return sum;
+            }
+            Console.Write("Введите целое число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(getSumOfDigits(number));
+        }
+    }
+}
