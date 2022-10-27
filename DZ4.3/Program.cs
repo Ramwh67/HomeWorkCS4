19a07faeb51ca3f27b8d161ab7088f617d94b024
Ramwh <vasilkovd_67@mail.ru>
2022-10-27 21:27:02 +0400

    Добавлено решение третьей задачи

---
+++ DZ4.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[] createNewArray(int size)
+            {
+               int[] array = new int[size];
+               for (int i=0; i<size; i++)
+               {
+                    array[i] = new Random().Next(0,100);
+               }
+               return array;
+            }
+            
+            void printArray(int[] array)
+            {
+                for (int i=0; i<array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+ 
+            Console.Write("Введите размерность массива: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = createNewArray(a);
+            printArray(numbers);
+        }
+    }
+}
